99353edada298e40f717cfd85bb1f20bd3712283
GPM <javiergenepaul@gmail.com>
2026-04-21 16:27:10 +0800

    first commit

---
+++ src/main/java/com/gpm/wos_dtr/WosDtrApplication.java
@@ -0,0 +1,13 @@
+package com.gpm.wos_dtr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WosDtrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WosDtrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gpm/wos_dtr/WosDtrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gpm.wos_dtr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WosDtrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
